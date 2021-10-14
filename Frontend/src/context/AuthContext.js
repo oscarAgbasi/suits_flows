084487647766317320b2696bd835af58ffe6442d
agbasi oscar <agbasi.oscar@gmail.com>
2021-10-14 00:24:06 -0400

    Frontend code update

--- Frontend/src/context/AuthContext.js
+++ Frontend/src/context/AuthContext.js
@@ -1,4 +1,5 @@
 import createDataContext from './createDataContext';
+import * as AppAuth from 'expo-app-auth';
 
 const authReducer = (state, action) => {
   switch (action.type) {
@@ -38,6 +39,7 @@ const signin = dispatch => {
 };
 
 const signout = dispatch => {
+  //this.signOutAsync()
   return () => {
     dispatch({type: 'signout'});
   };
@@ -48,3 +50,61 @@ export const {Provider, Context} = createDataContext(
   {signin, signout, signup},
   {token: null, email: '', user: null},
 );
+
+
+let config = {
+  issuer: 'https://accounts.google.com',
+  scopes: ['profile'],
+  /* This is the CLIENT_ID generated from a Firebase project */
+  clientId: '794170349007-7fh6l1r82u4s3galdt6gpnb0qmam81jq.apps.googleusercontent.com',
+};
+
+
+let StorageKey = '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';
+
+export async function signInAsync() {
+  try{
+    let authState = await AppAuth.authAsync(config);
+    //await cacheAuthAsync(authState);
+    console.log('signInAsync', authState);
+    return authState;
+  } catch (err) {
+    console.log('Error from Sign in' );
+    console.dir(err);
+  }
+}
+
+// async function cacheAuthAsync(authState) {
+//   return await AsyncStorage.setItem(StorageKey, JSON.stringify(authState));
+// }
+
+// export async function getCachedAuthAsync() {
+//   let value = await AsyncStorage.getItem(StorageKey);
+//   let authState = JSON.parse(value);
+//   console.log('getCachedAuthAsync', authState);
+//   if (authState) {
+//     if (checkIfTokenExpired(authState)) {
+//       return refreshAuthAsync(authState);
+//     } else {
+//       return authState;
+//     }
+//   }
+//   return null;
+// }
+
+// function checkIfTokenExpired({ accessTokenExpirationDate }) {
+//   return new Date(accessTokenExpirationDate) < new Date();
+// }
+
+// export async function signOutAsync({ accessToken }) {
+//   try {
+//     await AppAuth.revokeAsync(config, {
+//       token: accessToken,
+//       isClientIdProvided: true,
+//     });
+//     await AsyncStorage.removeItem(StorageKey);
+//     return null;
+//   } catch (e) {
+//     alert(`Failed to revoke token: ${e.message}`);
+//   }
+// }
